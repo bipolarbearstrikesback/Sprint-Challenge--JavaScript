b002de43771fc44acd952e35016c76ecba21aac0
bipolarbearstrikesback <49734722+bipolarbearstrikesback@users.noreply.github.com>
2019-05-10 11:27:39 -0500

    Update functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,32 +1,44 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
-/* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
+/* Step 1: Create a function that accepts a callback
+  * Create a function named consume that can take 3 parameters.
+  * The first two parameteres can accept any argument
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x, y, callback)
+{
+  return callback(x, y)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(){ 
+  return x + y
+}
+function multiply(){
+  return x * y
+}
+function greeting(){
+  return `Hello ${x} ${y}`
+}
 
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+/* Step 3: Check your work by uncommenting the following calls to consume(): */
+   consume(2,2,add); // 4
+   consume(10,16,multiply); // 160
+   consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
-// ==== Closures ==== 
-
+// ==== Closures ====
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
-
+// Explanation:
+//It is because nestedfunction() is within the same scope as internal . you cant access it globally because of scope.
 
 const external = "I'm outside the function";
 
